06969a6b8f38fb1077576dfb8aa8fc66d93d9c1a
ParadoxDev <80007621+Paradox696969@users.noreply.github.com>
2023-03-27 03:54:49 +0100

    Fixed error with inequal array sizes

--- CSVConverter.py
+++ CSVConverter.py
@@ -4,5 +4,13 @@ import json
 with open('data.json') as f:
     data = json.load(f)
 
+biggestLen = 0
+for i in data:
+    biggestLen = len(data[i]) if len(data[i]) > biggestLen else biggestLen
+
+for i in data:
+    for j in range(biggestLen - len(data[i])):
+        data[i].append('N/A')
+
 df = pandas.DataFrame(data)
 df.to_csv('results.csv', index=False)
